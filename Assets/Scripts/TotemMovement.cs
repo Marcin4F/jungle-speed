0191869c7420203ac836905ebed9e722437ae746
Marcin4F <markost4f@gmail.com>
2025-12-27 13:41:24 +0100

    wracanie totemu na srodek

--- Assets/Scripts/TotemMovement.cs
+++ Assets/Scripts/TotemMovement.cs
@@ -18,16 +18,16 @@ public class TotemMovement : MonoBehaviour
         switch(id)      // przesuniecie do odpowiedniego gracza
         {
             case 0:
-                targetPosition = startPosition + new Vector3(2, 0, -5.25f);
+                targetPosition = new Vector3(2, 1, -5.25f);
                 break;
             case 1:
-                targetPosition = startPosition + new Vector3(-9, 0, -2);
+                targetPosition = new Vector3(-9, 1, -2);
                 break;
             case 2:
-                targetPosition = startPosition + new Vector3(-2, 0, 5.25f);
+                targetPosition = new Vector3(-2, 1, 5.25f);
                 break;
             case 3:
-                targetPosition = startPosition + new Vector3(9, 0, 2);
+                targetPosition = new Vector3(9, 1, 2);
                 break;
         }
 
@@ -41,5 +41,26 @@ public class TotemMovement : MonoBehaviour
             yield return null;
         }
         transform.position = targetPosition;    // poprawienie pozycji
+
+        StartCoroutine(MovingTotemBack());
+    }
+
+    private IEnumerator MovingTotemBack()
+    {
+        yield return new WaitForSeconds(4);
+
+        startPosition = transform.position;
+        targetPosition = new Vector3(0, 1, 0);
+
+        while (elapsedTime < duration)      // przesuwanie totemu
+        {
+            elapsedTime += Time.deltaTime;
+            float t = elapsedTime / duration;
+
+            transform.position = Vector3.Lerp(startPosition, targetPosition, t);
+
+            yield return null;
+        }
+        transform.position = targetPosition;
     }
 }
